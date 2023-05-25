aeeea289b319abaa149014d0000b4d439fdcdb8d
Hilman Shobari <hilmanshobari7@gmail.com>
2023-05-25 14:32:12 +0700

    add Name of owner

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test Edit <code>src/App.js</code> and save to reload.
+          Test Edit <code>src/App.js</code> Hilman Ges.
         </p>
         <a
           className="App-link"
